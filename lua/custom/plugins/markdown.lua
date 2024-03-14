6684a14ae33af1e7b205af2992e4c9707aee1f4d
Dzmitry Mikhalapau <dzmitry.mikhalapau@gmail.com>
2024-03-14 18:08:21 +0500

    Install markdown plugin

---
+++ lua/custom/plugins/markdown.lua
@@ -0,0 +1,12 @@
+local packages = {
+  {
+    'iamcco/markdown-preview.nvim',
+    cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
+    ft = { 'markdown' },
+    build = function()
+      vim.fn['mkdp#util#install']()
+    end,
+  },
+}
+
+return packages
