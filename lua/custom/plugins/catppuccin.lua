d9cc70bf82cfb34f93b872e4cd5cca131a672dc9
Dzmitry Mikhalapau <dzmitry.mikhalapau@gmail.com>
2024-04-03 12:40:14 +0500

    Setup catppuccin theme as the default one

--- lua/custom/plugins/catppuccin.lua
+++ lua/custom/plugins/catppuccin.lua
@@ -5,37 +5,38 @@ local packages = {
     lazy = false,
     priority = 1000,
     config = function()
-      vim.cmd [[colorscheme catppuccin]]
-    end,
-    opts = {
-      flavour = 'macchiato',
-      integrations = {
-        barbar = true,
-        cmp = true,
-        gitsigns = true,
-        nvimtree = true,
-        treesitter = true,
-        mason = true,
-        native_lsp = {
-          enabled = true,
-          virtual_text = {
-            errors = { 'italic' },
-            hints = { 'italic' },
-            warnings = { 'italic' },
-            information = { 'italic' },
-          },
-          underlines = {
-            errors = { 'underline' },
-            hints = { 'underline' },
-            warnings = { 'underline' },
-            information = { 'underline' },
-          },
-          inlay_hints = {
-            background = true,
+      require('catppuccin').setup {
+        flavour = 'macchiato',
+        integrations = {
+          barbar = true,
+          cmp = true,
+          gitsigns = true,
+          nvimtree = true,
+          treesitter = true,
+          mason = true,
+          native_lsp = {
+            enabled = true,
+            virtual_text = {
+              errors = { 'italic' },
+              hints = { 'italic' },
+              warnings = { 'italic' },
+              information = { 'italic' },
+            },
+            underlines = {
+              errors = { 'underline' },
+              hints = { 'underline' },
+              warnings = { 'underline' },
+              information = { 'underline' },
+            },
+            inlay_hints = {
+              background = true,
+            },
           },
         },
-      },
-    },
+      }
+
+      vim.cmd.colorscheme 'catppuccin'
+    end,
   },
 }
 
