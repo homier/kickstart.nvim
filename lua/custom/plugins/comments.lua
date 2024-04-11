8283cb4df606541fa604aff8784f38556a9b3f03
Dzmitry Mikhalapau <dzmitry.mikhalapau@gmail.com>
2024-04-11 15:29:14 +0500

    Split lazy plugins configuration onto separate files

---
+++ lua/custom/plugins/comments.lua
@@ -0,0 +1,9 @@
+local packages = {
+  -- "gc" to comment visual regions/lines
+  { 'numToStr/Comment.nvim', opts = {} },
+
+  -- Highlight todo, notes, etc in comments
+  { 'folke/todo-comments.nvim', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
+}
+
+return packages
