c1d030f91ffb72ac0ab6a3dd890de371d3adaa09
Dzmitry Mikhalapau <dzmitry.mikhalapau@gmail.com>
2024-11-07 20:03:35 +0500

    Manipulate nvim buffers using telescope
    Signed-off-by: Dzmitry Mikhalapau <dzmitry.mikhalapau@gmail.com>

--- lua/custom/plugins/telescope.lua
+++ lua/custom/plugins/telescope.lua
@@ -70,6 +70,7 @@ local packages = {
 
       -- See `:help telescope.builtin`
       local builtin = require 'telescope.builtin'
+
       vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
       vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
       vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
@@ -79,7 +80,7 @@ local packages = {
       vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
-      vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+      vim.keymap.set('n', '<leader><leader>', buffer_searcher, { desc = '[ ] Find existing buffers' })
 
       -- Slightly advanced example of overriding default behavior and theme
       vim.keymap.set('n', '<leader>/', function()
@@ -106,5 +107,44 @@ local packages = {
     end,
   },
 }
+
+buffer_searcher = function()
+  local builtin = require 'telescope.builtin'
+  local action_state = require 'telescope.actions.state'
+  local actions = require 'telescope.actions'
+
+  builtin.buffers {
+    sort_mru = true,
+    ignore_current_buffer = true,
+    show_all_buffers = false,
+    attach_mappings = function(prompt_bufnr, map)
+      local refresh_buffer_searcher = function()
+        actions.close(prompt_bufnr)
+        vim.schedule(buffer_searcher)
+      end
+
+      local delete_buf = function()
+        local selection = action_state.get_selected_entry()
+        vim.api.nvim_buf_delete(selection.bufnr, { force = true })
+        refresh_buffer_searcher()
+      end
+
+      local delete_multiple_buf = function()
+        local picker = action_state.get_current_picker(prompt_bufnr)
+        local selection = picker:get_multi_selection()
+        for _, entry in ipairs(selection) do
+          vim.api.nvim_buf_delete(entry.bufnr, { force = true })
+        end
+        refresh_buffer_searcher()
+      end
+
+      map('n', 'dd', delete_buf)
+      map('n', '<C-d>', delete_multiple_buf)
+      map('i', '<C-d>', delete_multiple_buf)
+
+      return true
+    end,
+  }
+end
 
 return packages
